b84f905398234f308a4f702f9d83796cc1d7447e
yopox <dev@yopox.fr>
2025-04-05 20:57:32 +0200

    [worm] dig dirt

--- actors/worm/worm.gd
+++ actors/worm/worm.gd
@@ -1,10 +1,12 @@
 class_name Worm extends Node2D
 
 @onready var parts: Node2D = $Parts
-@onready var color_rect: ColorRect = $ColorRect
 
-var head_pos = Vector2(0.0, 0.0)
-var speed = 0.3
+var speed = 0.5
+var head_pos = Vector2.ZERO
+
+var dirt = preload("res://actors/worm/dirt.tscn")
+
 
 func _process(delta: float) -> void:
 	move()
@@ -21,19 +23,30 @@ func move():
 	var move = Vector2(dx, dy).normalized() * speed
 	var dist = speed
 	
-	if dist > 0.001:
-		head_pos += move
-		moved = true
-
-	if moved:
-		color_rect.position = head_pos
-		var prev_head = head_pos
-		var z = Util.Z_WORM + parts.get_children().size()
-		for section: WormSection in parts.get_children():
-			var temp = section.position
-			if section.position.distance_to(prev_head) > 3.0:
-				var angle = temp.angle_to_point(prev_head)
-				section.position = temp + Vector2.from_angle(angle) * dist
-			section.z_index = z
-			z -= 1
-			prev_head = temp
+	if not dist > 0.001: return
+	
+	position += move
+	parts.position -= move
+	head_pos += move
+	
+	var prev_head = head_pos
+	var z = Util.Z_WORM + parts.get_children().size()
+	
+	for section: WormSection in parts.get_children():
+		var temp = section.position
+		if section.position.distance_to(prev_head) > 3.0:
+			var angle = temp.angle_to_point(prev_head)
+			section.position = temp + Vector2.from_angle(angle) * dist
+		section.z_index = z
+		z -= 1
+		
+		# Make dirt dark
+		var dirt_mark: Node2D = dirt.instantiate()
+		dirt_mark.position = temp
+		Util.dirt.add_child(dirt_mark)
+		
+		prev_head = temp
+	
+	var dirt_marks = Util.dirt.get_children()
+	for i in range(0, dirt_marks.size() - 200 * parts.get_children().size()):
+		dirt_marks[i].queue_free()
--- globals/util.gd
+++ globals/util.gd
@@ -4,6 +4,8 @@ enum Scenes { Title, Game }
 
 var scene_manager: SceneManager
 
+var dirt: Node2D
+
 var scene_paths: Dictionary[Scenes, String] = {
 	Scenes.Title: "res://menus/title/title.tscn",
 	Scenes.Game: "res://scenes/game/game.tscn",
