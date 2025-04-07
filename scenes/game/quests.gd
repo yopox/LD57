f2b402dbe02dc849a4c62b42dcbe8ef175137a39
yopox <dev@yopox.fr>
2025-04-07 23:38:26 +0200

    [gui] add tracker

--- scenes/game/quests.gd
+++ scenes/game/quests.gd
@@ -1,9 +1,16 @@
 class_name Quests extends Node
 
+@onready var heart_1: Sprite2D = $"../CanvasLayer/Progress/Heart1"
+@onready var heart_2: Sprite2D = $"../CanvasLayer/Progress/Heart2"
+@onready var heart_3: Sprite2D = $"../CanvasLayer/Progress/Heart3"
+@onready var heart_4: Sprite2D = $"../CanvasLayer/Progress/Heart4"
+@onready var heart_5: Sprite2D = $"../CanvasLayer/Progress/Heart5"
+@onready var heart_6: Sprite2D = $"../CanvasLayer/Progress/Heart6"
+
+@export var waiting_caterpillar: Caterpillar
 
 var following_caterpillar: Node2D = null
 var saved_caterpillars: int = 0 : set = _set_saved_caterpillars
-@export var waiting_caterpillar: Caterpillar
 
 var helped: Dictionary[Name, Variant] = {}
 
@@ -19,6 +26,19 @@ enum Name {
 
 func complete_quest(name: Name) -> void:
 	helped[name] = 1
+	match name:
+		Name.FIREFLY:
+			heart_1.visible = true
+		Name.CATERPILLARS:
+			heart_2.visible = true
+		Name.ANT:
+			heart_3.visible = true
+		Name.BIRD:
+			heart_4.visible = true
+		Name.MOLE:
+			heart_5.visible = true
+		Name.WORM:
+			heart_6.visible = true
 
 
 func is_completed(name: Name) -> bool:
