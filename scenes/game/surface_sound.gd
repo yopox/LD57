6bfa6a0fa503bf37a486b32c47f46febfb98a40f
yopox <dev@yopox.fr>
2025-04-07 18:00:14 +0200

    [audio] remove audio effect
    In Godot 4.4 audio effects are not supported in the web export :(

--- scenes/game/surface_sound.gd
+++ scenes/game/surface_sound.gd
@@ -2,13 +2,13 @@ extends AudioStreamPlayer
 
 @onready var player: Node2D = $"../Player"
 
-var lpf: AudioEffectFilter
-
-
-func _ready() -> void:
-	lpf = AudioServer.get_bus_effect(1, 0)
+#var lpf: AudioEffectFilter
+#
+#
+#func _ready() -> void:
+	#lpf = AudioServer.get_bus_effect(1, 0)
 
 func _process(_delta: float) -> void:
 	var y = player.global_position.y
-	lpf.cutoff_hz = max(0, 8000 - y * 60)
-	volume_linear = clamp(1 - y / 200, 0, 1)
+	#lpf.cutoff_hz = max(0, 8000 - y * 60)
+	volume_linear = clamp(1 - y / 100, 0, 1)
